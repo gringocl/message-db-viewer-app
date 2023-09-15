395e3f84e0ec01c25b687ba9acb8ab6f143c2b8d
Scott Abbott <scott.abbott.a@gmail.com>
2023-09-14 23:15:34 -0600

    Initial implementation of active stream names

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,39 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+use sqlx::FromRow;
+use sqlx::{postgres::PgPoolOptions, PgPool};
+use tauri::{command, State};
+
+#[command]
+async fn active_stream_names(pool: State<'_, PgPool>) -> Result<Vec<String>, ()> {
+    #[derive(FromRow)]
+    pub struct StreamName(String);
+
+    let stream_names: Vec<StreamName> = sqlx::query_as(
+    "SELECT DISTINCT stream_name, MAX(global_position) FROM messages GROUP BY stream_name ORDER BY max(global_position) DESC LIMIT 25"
+    )
+    .fetch_all(pool.inner()).await.expect("Stream names from message store");
+
+    let stream_name_strings = stream_names
+        .into_iter()
+        .map(|StreamName(name)| name)
+        .collect();
+
+    Ok(stream_name_strings)
+}
+
+#[tokio::main]
+async fn main() {
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect("postgres://message_store:@localhost/message_store")
+        .await
+        .expect("Pool to be connected");
+
+    tauri::Builder::default()
+        .manage(pool)
+        .invoke_handler(tauri::generate_handler![active_stream_names])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
--- src/components/stream_name_panel/active_streams.tsx
+++ src/components/stream_name_panel/active_streams.tsx
@@ -2,6 +2,8 @@ import { addableStreamNames } from "@lib/stream_names"
 import React from "react"
 import useSWR from "swr"
 import { StreamNameList } from "@components/stream_name_panel/stream_name_list"
+import { useEffect, useState } from 'react'
+import { invoke } from '@tauri-apps/api/tauri'
 
 const fetchJSON = (url: string) => fetch(url).then((res) => res.json())
 
@@ -10,13 +12,13 @@ export const ActiveStreams = ({
 }: {
   selectedStreamNames: string[]
 }) => {
-  let { data: activeStreamNames } = useSWR<string[]>(
-    `/api/active-streams`,
-    fetchJSON,
-    {
-      refreshInterval: 500,
-    }
-  )
+  let [activeStreamNames, setActiveStreamNames] = useState(["some-stream"])
+
+  useEffect(() => {
+    invoke<string>('active_stream_names', { })
+      .then((stream_names) => setActiveStreamNames(stream_names))
+      .catch(console.error)
+  }, [])
 
   if (!activeStreamNames) return null
 
